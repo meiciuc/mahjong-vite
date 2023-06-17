bc51ac0707225920eec51cdb890739f29808efa6
meiciuc <meiciuc@gmail.com>
2023-06-18 01:54:29 +0300

    cleaning

--- src/ecs/tiles/TileInteractiveSystem.ts
+++ src/ecs/tiles/TileInteractiveSystem.ts
@@ -1,7 +1,4 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable prettier/prettier */
 import { Engine, NodeList, System } from "@ash.ts/ash";
-// import { GameStateEnum } from "../GameStateEnum";
 import { GameNode } from "../game/nodes/GameNode";
 import { TileNode } from "./nodes/TileNode";
 import { TileSelectedNode } from "./nodes/TileSelectedNode";
@@ -13,8 +10,6 @@ export class TileInteractiveSystem extends System {
     private tiles?: NodeList<TileNode>;
     private tilesSelected?: NodeList<TileSelectedNode>;
     private game?: NodeList<GameNode>;
-
-    // private lastGameObjectPointedDown: Phaser.GameObjects.GameObject | undefined;
 
     constructor(
         private creator: EntityCreator
@@ -29,33 +24,15 @@ export class TileInteractiveSystem extends System {
         this.tilesSelected = engine.getNodeList(TileSelectedNode);
 
         this.game = engine.getNodeList(GameNode);
-
-        // stageService.stage.input.on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, this.handleGameObjectDown);
-        // stageService.stage.input.on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handleGameObjectUp);
     }
 
     removeFromEngine(_engine: Engine): void {
-        // this.tiles.nodeAdded.remove(this.handleTileAdded);
-
-        // stageService.stage.input.off(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, this.handleGameObjectDown);
-        // stageService.stage.input.off(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, this.handleGameObjectUp);
     }
 
     update(_time: number): void {
-        
     }
 
     private handleTileAdded = (node: TileNode) => {
-        // node.display.view.setInteractive(
-        //     new Phaser.Geom.Rectangle(
-        //         0,
-        //         0, 
-        //         Config.ICON_IMAGE_WIDTH, 
-        //         Config.ICON_IMAGE_HEIGHT
-        //     ),
-        //     Phaser.Geom.Rectangle.Contains
-        // );
-        // node.display.view.input.cursor = 'pointer';
         node.display.view.interactive = true;
         node.display.view.cursor = 'pointer';
         node.display.view.on('mouseup', this.handleClick)
@@ -72,32 +49,6 @@ export class TileInteractiveSystem extends System {
             }
         }
     }
-
-    // private handleGameObjectDown = (pointer: Phaser.Input.Pointer, arr: Array<any>) => {
-    //     if (!this.isHandable()) {
-    //         return;
-    //     }
-    //     this.lastGameObjectPointedDown = arr[0];
-    // }
-
-    // private handleGameObjectUp = (pointer: Phaser.Input.Pointer, arr: Array<any>) => {
-    //     if (!this.isHandable()) {
-    //         return;
-    //     }
-    //     const temp = this.lastGameObjectPointedDown;
-    //     this.lastGameObjectPointedDown = undefined;
-
-    //     const target = arr[0];
-    //     if (temp !== target) {
-    //         return;
-    //     }
-        
-    //     for (let node = this.tiles.head; node; node = node.next) {
-    //         if (node.display.view === target) {
-    //             this.clickTile(node);
-    //         }
-    //     }
-    // }
 
     private clickTile(node: TileNode) {
         for (let selected = this.tilesSelected?.head; selected; selected = selected.next) {
