b4a7d5580eefb61e599fc9aa311eead01ec4aac8
meiciuc <meiciuc@gmail.com>
2023-12-24 03:12:45 +0300

    states

--- src/model/GameModel.ts
+++ src/model/GameModel.ts
@@ -10,12 +10,24 @@ export enum GameStateEnum {
 
 export enum AppStateEnum {
     NONE = 'none',
+
+    START_SCREEN_FIRST = 'startScreenFirst',
+    START_SCREEN_NOVICE = 'startScreenNovice',
     START_SCREEN = 'startScreen',
+
     GAME_SCREEN = 'gameScreen',
+    GAME_SCREEN_PAUSE = 'gameScreenPause',
+
     GAME_VICTORY = 'gameVictoryScreen',
-    GAME_DEFEATED = 'gameDefeateScreen',
+    GAME_VICTORY_NEXT_CHOOSING = 'gameVictoryScreenNextChoosing',
+
     GAME_NO_MORE_MOVES = 'gameNoMoreMovesScreen',
-    PAUSE_WHILE_ADS = 'pauseWhileAds',
+    GAME_NO_MORE_MOVES_ADS = 'gameNoMoreMovesAds',
+    GAME_NO_MORE_MOVES_CHOOSING = 'gameNoMoreMovesChoosing',
+
+    GAME_DEFEATED = 'gameDefeateScreen',
+    GAME_DEFEATED_ADS = 'gameDefeatedAds',
+    GAME_DEFEATED_CHOOSING = 'gameDefetedChoosing',
 }
 
 export interface IconState {
