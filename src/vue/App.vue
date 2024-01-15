a9a2ec19631e4576ab99cc4834a87847eca63ee4
meiciuc <meiciuc@gmail.com>
2024-01-15 19:14:06 +0300

    use component Transition

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -19,17 +19,21 @@ import { computed, ref } from 'vue';
 import isMobile from 'is-mobile';
 
 const appState = useModel(["appState"]);
-const optionsAreVisible = useModel(["optionsAreVisible"]);
 
 const isLeftMenu = ref(isMobile() && (window.innerWidth > window.innerHeight));
 
+const showGameOptionsButton = computed(() => {
+    return appState.value !== AppStateEnum.NONE && appState.value !== AppStateEnum.START_SCREEN_FIRST;
+});
+
+const optionsAreVisible = useModel(["optionsAreVisible"]);
+
+const showMainMenu = computed(() => {
+    return appState.value !== AppStateEnum.NONE && appState.value !== AppStateEnum.START_SCREEN_FIRST;
+});
+
 const showGameMenu = computed(() => {
     switch (appState.value) {
-        case AppStateEnum.NONE:
-        case AppStateEnum.START_SCREEN_FIRST:
-        case AppStateEnum.START_SCREEN_NOVICE:
-        case AppStateEnum.START_SCREEN:
-            return false;
         case AppStateEnum.GAME_SCREEN:
         case AppStateEnum.GAME_SCREEN_PAUSE:
         case AppStateEnum.GAME_VICTORY:
@@ -44,12 +48,14 @@ const showGameMenu = computed(() => {
             return false;
     }
 });
+
 const showModalBackground = computed(() => {
     return optionsAreVisible.value;
 });
 
 const showColoredBackground = computed(() => {
     switch (appState.value) {
+        case AppStateEnum.NONE:
         case AppStateEnum.START_SCREEN:
         case AppStateEnum.START_SCREEN_FIRST:
         case AppStateEnum.START_SCREEN_NOVICE:
@@ -69,31 +75,62 @@ window.addEventListener('resize', () => {
     <div id="canvas" :class="[isLeftMenu ? 'l-canvas' : 't-canvas']"></div>
 
     <GamePause v-if="appState === AppStateEnum.GAME_SCREEN_PAUSE"></GamePause>
-    <GameMenu v-show="showGameMenu" :class="[isLeftMenu ? 'l-menu' : 't-menu']"></GameMenu>
+    <GameMenu v-if="showGameMenu" :class="[isLeftMenu ? 'l-menu' : 't-menu']"></GameMenu>
 
-    <ModalBackgroundColored v-show="showColoredBackground"></ModalBackgroundColored>
+    <Transition>
+        <ModalBackgroundColored v-if="showColoredBackground"></ModalBackgroundColored>
+    </Transition>
 
-    <StartScreen v-if="appState === AppStateEnum.START_SCREEN"></StartScreen>
-    <StartScreenFirst v-if="appState === AppStateEnum.START_SCREEN_FIRST"></StartScreenFirst>
-    <StartScreenNovice v-if="appState === AppStateEnum.START_SCREEN_NOVICE"></StartScreenNovice>
 
-    <GameVictoryScreen v-if="appState === AppStateEnum.GAME_VICTORY"></GameVictoryScreen>
-    <GameDefeatedScreen v-if="appState === AppStateEnum.GAME_DEFEATED"></GameDefeatedScreen>
-    <GameDefeatedScreenFullscreenAds v-if="appState === AppStateEnum.GAME_DEFEATED_ADS"></GameDefeatedScreenFullscreenAds>
-    <NoMoreMovesScreen v-if="appState === AppStateEnum.GAME_NO_MORE_MOVES"></NoMoreMovesScreen>
+    <Transition>
+        <StartScreen v-if="appState === AppStateEnum.START_SCREEN"></StartScreen>
+    </Transition>
+    <Transition>
+        <StartScreenFirst v-if="appState === AppStateEnum.START_SCREEN_FIRST"></StartScreenFirst>
+    </Transition>
+    <Transition>
+        <StartScreenNovice v-if="appState === AppStateEnum.START_SCREEN_NOVICE"></StartScreenNovice>
+    </Transition>
 
-    <GameMenuMain v-if="appState !== AppStateEnum.START_SCREEN_FIRST" :class="[isLeftMenu ? 'l-menu-main' : 't-menu-main']">
+    <Transition>
+        <GameVictoryScreen v-if="appState === AppStateEnum.GAME_VICTORY"></GameVictoryScreen>
+    </Transition>
+
+    <Transition>
+        <GameDefeatedScreen v-if="appState === AppStateEnum.GAME_DEFEATED"></GameDefeatedScreen>
+    </Transition>
+    <Transition>
+        <GameDefeatedScreenFullscreenAds v-if="appState === AppStateEnum.GAME_DEFEATED_ADS">
+        </GameDefeatedScreenFullscreenAds>
+    </Transition>
+    <Transition>
+        <NoMoreMovesScreen v-if="appState === AppStateEnum.GAME_NO_MORE_MOVES"></NoMoreMovesScreen>
+    </Transition>
+    <GameMenuMain v-if="showMainMenu" :class="[isLeftMenu ? 'l-menu-main' : 't-menu-main']">
     </GameMenuMain>
-
-    <ModalBackground v-show="showModalBackground"></ModalBackground>
-    <Options v-if="optionsAreVisible"></Options>
-
-    <GameMenuMainOptions v-if="appState !== AppStateEnum.START_SCREEN_FIRST"
-        :class="[isLeftMenu ? 'l-menu-main' : 't-menu-main']"></GameMenuMainOptions>
+    <Transition>
+        <ModalBackground v-if="showModalBackground"></ModalBackground>
+    </Transition>
+    <Transition>
+        <Options v-if="optionsAreVisible"></Options>
+    </Transition>
+    <GameMenuMainOptions v-if="showGameOptionsButton" :class="[isLeftMenu ? 'l-menu-main' : 't-menu-main']">
+    </GameMenuMainOptions>
 </template>
 
 <style lang="scss" scoped>
 // https://stackoverflow.com/questions/23870696/vertical-navigation-with-rotated-text
+
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
 
 // menu-left
 .l-menu-main {
