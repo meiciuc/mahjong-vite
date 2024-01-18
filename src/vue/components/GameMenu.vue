fd145891fad1d5733c5caa203a1205d942118cad
meiciuc <meiciuc@gmail.com>
2024-01-18 23:57:53 +0300

    component GameMenuHelp.vue

--- src/vue/components/GameMenu.vue
+++ src/vue/components/GameMenu.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import { VueServiceSignals, vueService } from '../VueService';
 import { useModel } from '../../model/useModel';
-import { Localization } from '../../utils/Localization';
 import GameMenuTimer from './GameMenuTimer.vue';
+import GameMenuHelp from './GameMenuHelp.vue';
 
-const helpsCount = useModel(["helpsCount"]);
 const gameCurrentScore = useModel(["gameCurrentScore"]);
 
-const handleClick = () => {
-    vueService.signalDataBus.dispatch(VueServiceSignals.HelpButton);
-}
 </script>
 
 <template>
     <div class="MenuPanel">
         <div class="GameScore">{{ gameCurrentScore }}</div>
 
-        <div>
-            <GameMenuTimer></GameMenuTimer>
-        </div>
-
-        <div class="HelpContainer">
-            <div v-if="helpsCount > 0" class="HelpButton" @click="handleClick">{{ Localization.getText('game.help') }}</div>
-            <div v-else class="HelpButton HelpButtonDisabled">{{ Localization.getText('game.help') }}</div>
-            <div class="HelpsCount">{{ helpsCount }}</div>
-        </div>
+        <GameMenuTimer></GameMenuTimer>
+        <GameMenuHelp></GameMenuHelp>
     </div>
 </template>
 
@@ -49,31 +37,6 @@ div {
 
 .GameScore {
     @include scene-text-block;
-    min-width: 6em;
-}
-
-.HelpContainer {
-    background-color: $background_colored_darken;
-    border-radius: $button_border_radius;
-}
-
-.HelpButton {
-    @include button;
-    padding: 0em 1em;
-}
-
-.HelpButton:hover {
-    @include button_hover;
-}
-
-.HelpButtonDisabled {
-    pointer-events: none;
-    cursor: auto;
-    opacity: 0.7;
-}
-
-.HelpsCount {
-    @include scene-text-block;
-    width: 3em;
+    min-width: 4em;
 }
 </style>
---
+++ src/vue/components/GameMenuHelp.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { useModel } from '../../model/useModel';
+import { Localization } from '../../utils/Localization';
+import { VueServiceSignals, vueService } from '../VueService';
+
+const helpsCount = useModel(["helpsCount"]);
+const handleClick = () => {
+    vueService.signalDataBus.dispatch(VueServiceSignals.HelpButton);
+}
+
+</script>
+
+<template>
+    <div class="HelpContainer">
+        <div v-if="helpsCount > 0" class="HelpButton" @click="handleClick">{{ Localization.getText('game.help') }}</div>
+        <div v-else class="HelpButton HelpButtonDisabled">{{ Localization.getText('game.help') }}</div>
+        <div class="HelpsCount">{{ helpsCount }}</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../global.scss';
+
+.HelpContainer {
+    background-color: $background_colored_darken;
+    border-radius: $button_border_radius;
+}
+
+.HelpButton {
+    @include button;
+    padding: 0em 1em;
+}
+
+.HelpButton:hover {
+    @include button_hover;
+}
+
+.HelpButtonDisabled {
+    pointer-events: none;
+    cursor: auto;
+    opacity: 0.7;
+}
+
+.HelpsCount {
+    @include scene-text-block;
+    width: 3em;
+}
+</style>
