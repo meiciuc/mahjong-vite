c87c455a49292e81eb447151bf7e73c90183fd0d
meiciuc <meiciuc@gmail.com>
2024-02-28 02:10:09 +0300

    tutorial video

--- src/vue/components/Tutorial.vue
+++ src/vue/components/Tutorial.vue
@@ -3,7 +3,10 @@
 <template>
     <div class="Tutorial">
         <div class="Wrapper">
-            <iframe class="IFrame" src="./assets/tutorial/index_HTML5 Canvas.html"></iframe>
+            <!-- <iframe class="IFrame" src="./assets/tutorial/index_HTML5 Canvas.html"></iframe> -->
+            <video autoplay muted loop class="Video">
+                <source src="../../../assets/tutorial/tutorial.webm" type="video/webm">
+            </video>
         </div>
     </div>
 </template>
@@ -12,10 +15,20 @@
 @import '../global.scss';
 
 .Tutorial .Wrapper {
-    width: 384px;
-    height: 384px;
-    padding: 0px;
+    position: relative;
+    width: 28rem;
+    height: 28rem;
     overflow: hidden;
+    background-color: cadetblue;
+}
+
+.Tutorial .Video {
+    position: absolute;
+    margin-left: -4rem;
+    margin-top: -4rem;
+    // transform: scale(.5);
+    width: 38rem;
+    height: 38rem;
 }
 
 .Tutorial .IFrame {
